e81a27b4497c3a2d383485503c9ba1b594ef4e5c
libailing <calipos@126.com>
2018-10-22 22:22:18 +0800

    check ringBuffer

--- oneShot/main.cpp
+++ oneShot/main.cpp
@@ -5,9 +5,29 @@
 #include "rapidjson/document.h"
 #include "rapidjson/prettywriter.h"
 #include "rapidjson/stringbuffer.h"
+
+
+#include"opencv2/opencv.hpp"
+
 int main()
 {
 
+	unre::FrameRingBuffer<uchar> buffer(400,300,3);
+	for (size_t i = 0; i < 5; i++)
+	{
+		cv::Mat img = cv::Mat::ones(400, 300, CV_8UC3)*i;
+		buffer.push(img.data);
+	}
+	cv::Mat poped = cv::Mat::ones(400, 300, CV_8UC3);
+	poped.data = buffer.pop();
+	cv::Mat img = cv::Mat::ones(400, 300, CV_8UC3)*6;
+	buffer.push(img.data);
+	while (!buffer.empty())
+	{
+		poped.data = buffer.pop();
+	}
+	
+	
 	rapidjson::Document docRoot;
 	docRoot.Parse<0>(unre::StringOP::parseJsonFile2str("E:/repo/oneShot/test.json").c_str());
 	if (!docRoot.HasParseError())
